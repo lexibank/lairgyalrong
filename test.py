9f8bd2fa2db49f5fb996e994f32634c3efe55a9f
lingulist <mattis.list@lingpy.org>
2023-04-26 12:01:19 +0200

    update

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ def test_valid(cldf_dataset, cldf_logger):
 
 
 def test_languages(cldf_dataset):
-    assert len(list(cldf_dataset['LanguageTable'])) == 18
+    assert len(list(cldf_dataset['LanguageTable'])) == 22
 
 
 def test_parameters(cldf_dataset):
-    assert len(list(cldf_dataset['ParameterTable'])) == 905
+    assert len(list(cldf_dataset['ParameterTable'])) == 305
 
 
 def test_sources(cldf_dataset):
-    assert len(cldf_dataset.sources) == 1
+    assert len(cldf_dataset.sources) == 22
 
